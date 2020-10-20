fa94312904210c736b6dc89716b002cc872b4ef2
rasolko <vano2029492@mail.ru>
2020-10-20 22:57:22 +0300

    feat: complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let number = n;
+  if(number < 0){
+    number = number * (-1);
+  }
+  let result = number.toString().split('').reverse().join('');
+  return +result
 }
